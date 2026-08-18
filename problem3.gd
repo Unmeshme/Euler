989609404b72356d9e89c91ca715d8f281d0d189
Unmeshme <unmesh.teslatech@gmail.com>
2026-08-18 10:15:43 +0545

    solution to problem 3

---
+++ problem3.gd
@@ -0,0 +1,26 @@
+extends SceneTree
+
+func _init():
+    var start = Time.get_unix_time_from_system()
+    var solution = main() #yet to decide the footprint
+    var duration = Time.get_unix_time_from_system() - start
+
+    print("Solution: %s, duration: %.5f " %[solution, duration])
+    quit()
+
+func main() -> int:
+    return get_Sum_of_Even_fib()
+
+func get_Sum_of_Even_fib() -> int:
+    var first = 1
+    var second = 2
+    var total = second
+
+    while second < 4000000:
+        var new = first + second
+        if new & 1 == 0: #condition for even
+            total += new
+        first = second
+        second = new
+
+    return total
